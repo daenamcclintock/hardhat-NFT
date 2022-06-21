fbc04e40f71e10ec4225d7035442e65e3ba15018
daenamcclintock <daena.mcclintock@gmail.com>
2022-06-21 09:47:01 -0400

    added script to upload to NFT Storage

--- deploy/02-deploy-random-ipfs.js
+++ deploy/02-deploy-random-ipfs.js
@@ -5,6 +5,7 @@ const { verify } = require("../utils/verify")
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
 
     log("----------------------------------------------------")
 
